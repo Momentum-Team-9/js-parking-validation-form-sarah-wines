ee03eaaa297f5fff49a7b9c35f794400bd45981f
sarah-wines <sarah.lodico.wines@gmail.com>
2021-07-17 17:52:11 -0400

    1-3 mostly working. Started number 4, got text to pop up

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Add validation!');
+const form = document.querySelector('#parking-form');
+const button = document.querySelector('#submit-button');
+const nameInput = document.getElementById('name');
+
+let formIsValid;
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const valid = document.createElement('div');
+  valid.id = 'message';
+  let total = (document.querySelector('#total').appendChild(valid).innerText =
+    '$5.Pay up bitch');
+});
